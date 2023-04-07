2975c570df30e603fb7e8b200460d57d87f7f134
jagadish <jagadishbisai02@gmail.com>
2023-04-07 07:38:03 +0000

    browser histgory app

---
+++ src/components/HistoryItem/index.js
@@ -0,0 +1,35 @@
+import './index.css'
+
+const HistoryItem = props => {
+  const {historyItem, deleteItem} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyItem
+
+  const onDelete = () => {
+    deleteItem(id)
+  }
+  return (
+    <li className="history-list">
+      <div className="list-item">
+        <p>{timeAccessed}</p>
+        <img src={logoUrl} alt="domain logo" />
+        <p>{title}</p>
+        <p>{domainUrl}</p>
+      </div>
+      <div>
+        <button
+          data-testid="delete"
+          type="button"
+          className="delete-btn"
+          onClick={onDelete}
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default HistoryItem
